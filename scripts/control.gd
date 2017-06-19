fcd2b7f742205c5899c6bbb2e259b31a4d1d9750
Sir Meeseeks <ginardfalgueras@gmail.com>
2017-06-19 15:06:29 +0200

    solved winner buttons

--- scripts/control.gd
+++ scripts/control.gd
@@ -30,13 +30,13 @@ func _on_buttonMenu_pressed():
 func _fixed_process(delta):
 	finished = checkWinners()
 	if (finished):
-		get_node("popup/background/window/announcment").set_text(winner+" wins!")
+		get_node("background/window/announcment").set_text(winner+" wins!")
 		if (winner == "Blue"):
-			get_node("popup/background/window/announcment").add_color_override("font_color", Color(0.22,0.26,0.91,1))
+			get_node("background/window/announcment").add_color_override("font_color", Color(0.22,0.26,0.91,1))
 		else:
-			get_node("popup/background/window/announcment").add_color_override("font_color", Color(0.96,0.3,0.3,1))
+			get_node("background/window/announcment").add_color_override("font_color", Color(0.96,0.3,0.3,1))
 		get_tree().set_pause(true)
-		get_node("popup").popup()
+		get_node("background").show()
 
 func _ready():
 	arrel = get_parent()
